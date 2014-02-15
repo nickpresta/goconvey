653f4905126d643dbc1aaf264f811b8aabfbacfe
Michael Whatcott <mdwhatcott@gmail.com>
2014-02-14 21:57:09 -0700

    First draft of a stacktrace/build output rewriting mechanism.

--- web/server/watcher/cache.go
+++ web/server/watcher/cache.go
@@ -1,6 +1,24 @@
+// This code builds up maps of source code line numbers allowing
+// code that has coverage statements to be compared with the original
+// source code. This is necessary when correcting stack traces and build
+// failure output generated from `go test -cover` in order to present it
+// to the tester in the UI. See the tests for more details.
+//
+// The inspiration for this code:
+//     https://groups.google.com/d/topic/golang-nuts/GBeHbrIOxU4/discussion
+//
+// Rob Pike's summary:
+//     I think it's pretty easy, but comme ci comme ça.
+//
 package watcher
 
-import "github.com/smartystreets/goconvey/web/server/contract"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/smartystreets/goconvey/web/server/contract"
+)
 
 type SourceCache struct {
 	fs        contract.FileSystem
@@ -9,17 +27,63 @@ type SourceCache struct {
 }
 
 func (self *SourceCache) Update(path string, sum int64) {
-	// relativePath := contract.ResolvePackageName(path)
-	// if self.checksums[relativePath] != sum {
-	// 	self.checksums[relativePath] = sum
-	// 	// TODO: reload file, which means we discard the old map and create a new one from scratch.
-	// }
+	relativePath := contract.ResolvePackageName(path)
+	self.checksums[relativePath] = sum
+	original, coverage, err := self.fs.ReadGo(path)
+	if err != nil {
+		return // just bail
+	}
+
+	rewrite := make(map[int]int)
+	self.files[relativePath] = rewrite
+	lines := strings.Split(original, "\n")
+	covered := strings.Split(coverage, "\n")
+	y := 0
+	for x, _ := range lines {
+		if strings.Contains(covered[y], "GoConvey__coverage__") {
+			y++
+		}
+		rewrite[y] = x
+		y++
+	}
+	fmt.Println(self.files)
 }
 
-func (self *SourceCache) Rewrite(stack string) string {
-	// for path, _ /*rewrite*/ := range self.files {
-	// 	if strings.Contains(stack, path) {
-	// 	}
-	// }
-	return ""
+func (self *SourceCache) Rewrite(output string) string {
+	preparedOutput := strings.Replace(output, "_test/", "", -1)
+	outputLines := strings.Split(preparedOutput, "\n")
+	for path, rewrite := range self.files {
+		if strings.Contains(preparedOutput, path) {
+			for number, line := range outputLines {
+				if strings.Contains(line, path) {
+					fields := strings.Split(line, ":")
+					numberWord := strings.Split(strings.TrimSpace(fields[1]), " ")[0]
+					referenceLineNumber, err := strconv.Atoi(numberWord)
+					if err != nil {
+						return output // just bail
+					}
+					prefix := ""
+					if strings.HasPrefix(line, "\t") {
+						prefix = "\t"
+					}
+					suffix := " "
+					if len(fields) > 2 {
+						suffix = ":" + fields[2]
+					}
+					correctLineNumber := rewrite[referenceLineNumber]
+					correctedLine := fmt.Sprintf("%s%s:%d%s", prefix, path, correctLineNumber, suffix)
+					outputLines[number] = correctedLine
+				}
+			}
+		}
+	}
+	return strings.Join(outputLines, "\n")
+}
+
+func NewSourceCache(fs contract.FileSystem) *SourceCache {
+	self := new(SourceCache)
+	self.fs = fs
+	self.checksums = make(map[string]int64)
+	self.files = make(map[string]map[int]int)
+	return self
 }
--- web/server/watcher/cache_test.go
+++ web/server/watcher/cache_test.go
@@ -4,18 +4,48 @@ import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/smartystreets/goconvey/web/server/system"
 )
 
 func TestCache(t *testing.T) {
+	var (
+		cache *SourceCache
+		fs    *system.FakeFileSystem
+	)
+
 	Convey("Subject: SourceCache", t, func() {
+		fs = system.NewFakeFileSystem()
+		cache = NewSourceCache(fs)
 
-		Convey("When a stack trace contains a reference to a file has been scanned", func() {
-			SkipConvey("The cache should correct the line numbers with the original line numbers", func() {
+		Convey("When build failure output contains a reference to a file that has been scanned", func() {
+			fs.Populate(actualFilePath, originalBuildFailureSource, coveredBuildFailureSource, nil)
+			cache.Update(actualFilePath, 42)
+			actual := cache.Rewrite(coverageBuildFailure)
 
+			Convey("The cache should correct the line numbers with the original line numbers", func() {
+				So(actual, ShouldEqual, expectedBuildFailure)
 			})
 		})
 
-		Convey("When a stack trace contains no references to known files", func() {
+		Convey("When build failure output contains no references to known files", func() {
+			actual := cache.Rewrite("blah blah blah")
+
+			Convey("It should return the same stack trace", func() {
+				So(actual, ShouldEqual, "blah blah blah")
+			})
+		})
+
+		Convey("When panic output contains a reference to a file that has been scanned", func() {
+			fs.Populate(actualFilePath, originalPanicSource, coveredPanicSource, nil)
+			cache.Update(actualFilePath, 42)
+			actual := cache.Rewrite(coveragePanic)
+
+			Convey("The cache should correct the line numbers with the original line numbers", func() {
+				So(actual, ShouldEqual, expectedPanic)
+			})
+		})
+
+		Convey("When panic output contains no references to known files", func() {
 			SkipConvey("It should return the same stack trace", func() {
 
 			})
@@ -24,27 +54,25 @@ func TestCache(t *testing.T) {
 	})
 }
 
-const originalSource = `package scratch
+const actualFilePath = "/root/gopath/src/github.com/smartystreets/goconvey/scratch/scratch.go"
 
-import "fmt"
+const originalBuildFailureSource = `package scratch
 
 func hi() {
 	a := []int{}
-	fmt.Println(a[123])
+	// fmt.Println(a[123])
 }
 `
 
-const instrumentedSource = `package scratch
-
-import "fmt"
+const coveredBuildFailureSource = `package scratch
 
 func hi() {
-	GoCover.Count[0] = 1
+	GoConvey__coverage__.Count[0] = 1
 	a := []int{}
-	fmt.Println(a[123])
+	// fmt.Println(a[123])
 }
 
-var GoCover = struct {
+var GoConvey__coverage__ = struct {
 	Count     [1]uint32
 	Pos       [3 * 1]uint32
 	NumStmt   [1]uint16
@@ -57,35 +85,103 @@ var GoCover = struct {
 	},
 }
 `
+
 const coverageBuildFailure = `# github.com/smartystreets/goconvey/scratch
 /var/folders/rw/z5hccl114lzbvjjrqfwr9mcr0000gn/T/go-build011729042/github.com/smartystreets/goconvey/scratch/_test/scratch.go:5: a declared and not used
 FAIL	github.com/smartystreets/goconvey/scratch [build failed]
+`
+
+const expectedBuildFailure = `# github.com/smartystreets/goconvey/scratch
+github.com/smartystreets/goconvey/scratch/scratch.go:4: a declared and not used
+FAIL	github.com/smartystreets/goconvey/scratch [build failed]
+`
+
+const originalPanicSource = `package scratch
+
+import "fmt"
+
+func hi() {
+	a := []int{}
+	fmt.Println(a[123])
+}
+`
+
+const coveredPanicSource = `package scratch
+
+import "fmt"
+
+func hi() {
+	GoConvey__coverage__.Count[0] = 1
+	a := []int{}
+	fmt.Println(a[123])
+}
+
+var GoConvey__coverage__ = struct {
+	Count     [1]uint32
+	Pos       [3 * 1]uint32
+	NumStmt   [1]uint16
+} {
+	Pos: [3 * 1]uint32{
+		5, 8, 0x2000b, // [0]
+	},
+	NumStmt: [1]uint16{
+		2, // 0
+	},
+}
 `
 
 const coveragePanic = `panic: runtime error: index out of range [recovered]
 	panic: runtime error: index out of range
 
 goroutine 3 [running]:
-runtime.panic(0xf5bc0, 0x23ff97)
-	/usr/local/go/src/pkg/runtime/panic.c:266 +0xb6
+runtime.panic(0x00, 0x00)
+	/usr/local/go/src/pkg/runtime/panic.c:266 +0x00
 testing.func·005()
-	/usr/local/go/src/pkg/testing/testing.go:385 +0xe8
-runtime.panic(0xf5bc0, 0x23ff97)
-	/usr/local/go/src/pkg/runtime/panic.c:248 +0x106
+	/usr/local/go/src/pkg/testing/testing.go:385 +0x00
+runtime.panic(0x00, 0x00)
+	/usr/local/go/src/pkg/runtime/panic.c:248 +0x00
 github.com/smartystreets/goconvey/scratch.hi()
-	github.com/smartystreets/goconvey/scratch/_test/scratch.go:8 +0xd0
-github.com/smartystreets/goconvey/scratch.TestStuff(0x210333000)
-	/Users/mike/work/go/src/github.com/smartystreets/goconvey/scratch/scratch_test.go:6 +0x1a
-testing.tRunner(0x210333000, 0x23b080)
-	/usr/local/go/src/pkg/testing/testing.go:391 +0x8b
+	github.com/smartystreets/goconvey/scratch/_test/scratch.go:8 +0x00
+github.com/smartystreets/goconvey/scratch.TestScratch(0x00)
+	/Users/mike/work/go/src/github.com/smartystreets/goconvey/scratch/scratch_test.go:6 +0x00
+testing.tRunner(0x00, 0x00)
+	/usr/local/go/src/pkg/testing/testing.go:391 +0x00
 created by testing.RunTests
-	/usr/local/go/src/pkg/testing/testing.go:471 +0x8b2
+	/usr/local/go/src/pkg/testing/testing.go:471 +0x00
 
 goroutine 1 [chan receive]:
-testing.RunTests(0x1508f8, 0x23b080, 0x1, 0x1, 0xca201)
-	/usr/local/go/src/pkg/testing/testing.go:472 +0x8d5
-testing.Main(0x1508f8, 0x23b080, 0x1, 0x1, 0x242c80, ...)
-	/usr/local/go/src/pkg/testing/testing.go:403 +0x84
+testing.RunTests(0x00, 0x00, 0x00, 0x00, 0x00)
+	/usr/local/go/src/pkg/testing/testing.go:472 +0x00
+testing.Main(0x00, 0x00, 0x00, 0x00, 0x00, ...)
+	/usr/local/go/src/pkg/testing/testing.go:403 +0x00
 main.main()
-	github.com/smartystreets/goconvey/scratch/_test/_testmain.go:93 +0x11b
+	github.com/smartystreets/goconvey/scratch/_testmain.go:47 +0x00
+`
+
+const expectedPanic = `panic: runtime error: index out of range [recovered]
+	panic: runtime error: index out of range
+
+goroutine 3 [running]:
+runtime.panic(0x00, 0x00)
+	/usr/local/go/src/pkg/runtime/panic.c:266 +0x00
+testing.func·005()
+	/usr/local/go/src/pkg/testing/testing.go:385 +0x00
+runtime.panic(0x00, 0x00)
+	/usr/local/go/src/pkg/runtime/panic.c:248 +0x00
+github.com/smartystreets/goconvey/scratch.hi()
+	github.com/smartystreets/goconvey/scratch/scratch.go:7 
+github.com/smartystreets/goconvey/scratch.TestScratch(0x00)
+	/Users/mike/work/go/src/github.com/smartystreets/goconvey/scratch/scratch_test.go:6 +0x00
+testing.tRunner(0x00, 0x00)
+	/usr/local/go/src/pkg/testing/testing.go:391 +0x00
+created by testing.RunTests
+	/usr/local/go/src/pkg/testing/testing.go:471 +0x00
+
+goroutine 1 [chan receive]:
+testing.RunTests(0x00, 0x00, 0x00, 0x00, 0x00)
+	/usr/local/go/src/pkg/testing/testing.go:472 +0x00
+testing.Main(0x00, 0x00, 0x00, 0x00, 0x00, ...)
+	/usr/local/go/src/pkg/testing/testing.go:403 +0x00
+main.main()
+	github.com/smartystreets/goconvey/scratch/_testmain.go:47 +0x00
 `
